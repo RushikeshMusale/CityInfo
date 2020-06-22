805797fa4c3e90525c7d81dd44b7f98e363ba255
Rushikesh Musale <rushikeshmusale@gmail.com>
2020-06-22 10:04:26 +0530

    Added MVC to pipeline

--- CityInfo.API/Startup.cs
+++ CityInfo.API/Startup.cs
@@ -15,6 +15,7 @@ namespace CityInfo.API
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -25,11 +26,13 @@ namespace CityInfo.API
                 app.UseDeveloperExceptionPage();
             }
 
-            app.Run(async (context) =>
-            {
-                await context.Response.WriteAsync("Hello World!");
-                //throw new Exception("sample exception");
-            });
+            app.UseMvc();
+            //No longer required
+            //app.Run(async (context) =>
+            //{
+            //    await context.Response.WriteAsync("Hello World!");
+            //    //throw new Exception("sample exception");
+            //});
 
         }
     }
