2fe8bf8ad9fd249c1994020be7caec950de13833
Rushikesh Musale <rushikeshmusale@gmail.com>
2020-08-03 18:16:27 +0530

    Added CityInfo Context 1. Using Database.EnsureCreated 2. Using database migrations

---
+++ CityInfo.API/Contexes/CityInfoContext.cs
@@ -0,0 +1,33 @@
+﻿using CityInfo.API.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Data.Common;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CityInfo.API.Contexes
+{
+    public class CityInfoContext : DbContext
+    {
+        public DbSet<City> Cities { get; set; }
+
+        public DbSet<PointOfInterest> PointOfInterests { get; set; }
+
+        //Option2 - using datbaseOptions, configuration goes into configureservice()
+        public CityInfoContext(DbContextOptions options) : base(options)
+        {
+            // While performing update-database, it will create an instance of context which will create a database, 
+            // and in the migrations if we are trying to create tables, it will fail with below error:
+            // There is already an object named 'Cities' in the database.
+            //Database.EnsureCreated();
+        }
+
+        //Option1
+        //protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        //{
+        //    optionsBuilder.UseSqlServer("connectionString");
+        //    base.OnConfiguring(optionsBuilder);
+        //}
+    }
+}
